a32e3a99ba4fc0ee81421d6d9c62141941294c0b
brazeen <117746210+brazeen@users.noreply.github.com>
2024-04-15 21:58:45 +0800

    all working

--- Question4.java
+++ Question4.java
@@ -18,6 +18,13 @@ public class Question4
      */
      
     Scanner in = new Scanner(System.in);
+    int base = in.nextInt();
+    for (int i = 0; i < base; i++){
+      for (int j = base; j > i; j--){
+        System.out.print("*");
+      }
+      System.out.println();
+    }
     
   }
 }

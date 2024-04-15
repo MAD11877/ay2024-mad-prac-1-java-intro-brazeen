a24aa17cfd2d824b79960f4690ebf8155da2f311
brazeen <117746210+brazeen@users.noreply.github.com>
2024-04-15 22:08:13 +0800

    fixed comments

--- Question5.java
+++ Question5.java
@@ -25,7 +25,7 @@ public class Question5
         occurlist.add(count);
     }
     
-    //noww find highest count
+    //now find highest count
     int index = 0;
     int highest = 0;
     for (int i = 0; i < occurlist.size(); i++){
